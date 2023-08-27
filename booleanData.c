6894ff117ad0320b8ee8cda2d7624debd868422e
Kuruin <manavjit9@gmail.com>
2023-08-27 11:36:31 +0530

    Just dealing with boolean and its header file
    Day 4

--- booleanData.c
+++ booleanData.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
+#include <stdbool.h>
 int main()
 {
     // boolean is something wich true or false
-    _Bool isEarthFlat = 1;
+    _Bool isEarthFlat = true; // use this method to define without header file
+
     printf("Is the earth flat ?\n 1 for true \n 0 for false \n", isEarthFlat);
     scanf("%i", &isEarthFlat);
-    if (isEarthFlat == 1)
+    if (isEarthFlat == true)
     {
         printf("So the earth is flat ha.... ");
     }
