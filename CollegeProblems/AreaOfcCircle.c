6a6ae2225b10dc879619727b4e8697097ec55528
Kuruin <manavjit9@gmail.com>
2024-01-14 15:25:20 +0530

    College Problem area of circle

---
+++ CollegeProblems/AreaOfcCircle.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main (){
+    int r,area;
+    printf("Enter the value of radius for a circle");
+    scanf("%d",&r);
+    area=3.14*r*r;
+    printf("The area of the circle is %d",area);
+    return 0;
+}
