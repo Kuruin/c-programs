4f1b50312b76b9c8930048ede518686829eecdd1
Kuruin <manavjit9@gmail.com>
2024-01-27 13:58:17 +0530

    College Problems: Menu to perform
    1.Multiplication table \n 2.Factor of a no \n 3.Fibnoacci series upto a limit

---
+++ CollegeProblems/menu.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+int mult();
+int factorial();
+int fibonacci();
+void main()
+{
+    int num;
+    printf("Choose a menu:\n");
+    printf("1. Get multiplication table of the number\n");
+    printf("2. Get factorial of a number\n");
+    printf("3. Get Fibonacci series upto a given limit\n");
+    scanf("%d", &num);
+    switch (num)
+    {
+    case 1:
+        mult();
+        break;
+    case 2:
+        factorial();
+        break;
+    case 3:
+        fibonacci();
+        break;
+    default:
+        printf("Choose a valid menu");
+        break;
+    }
+}
+int mult()
+{
+    int n1, limit, table;
+    printf("Enter a number for table and the limit upto the table is required \n");
+    scanf("%d %d", &n1, &limit);
+    for (int i = 1; i <= limit; i++)
+    {
+        printf("The multiplication of %d is:\n", n1);
+        printf("%d * %d = %d\n", n1, i, table = n1 * i);
+    }
+    return 0;
+}
+int factorial()
+{
+    int num2, fact1 = 1;
+    printf("Enter a number\n");
+    scanf("%d", &num2);
+    for (int i = 1; i <= num2; i++)
+    {
+        fact1 = fact1 * i;
+    }
+    printf("The factorial is:%d", fact1);
+    return 0;
+}
+int fibonacci()
+{
+    int num3, nextNum, n4 = 0, n5 = 1;
+    printf("Enter the limit for fibonacci series \n");
+    scanf("%d", &num3);
+    printf("The fibonacci series is:\n");
+    for (int i = 1; i <= num3; ++i)
+    {
+        printf("%d \t", n4);
+        nextNum = n4 + n5;
+        n4 = n5;
+        n5 = nextNum;
+    }
+    return 0;
+
+    return 0;
+}
